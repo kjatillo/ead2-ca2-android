773154c20a7ed56f0a5b85c3aaf1cda926ca98f8
Ken <keneithatillo@gmail.com>
2025-04-29 22:41:14 +0100

    feat: add recipe repository

---
+++ EAD2_CA2.Api/Repositories/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EAD2_CA2.Api.Entities;
+
+namespace EAD2_CA2.Api.Repositories.Interfaces;
+
+public interface IRecipeRepository
+{
+    Task<IEnumerable<Recipe>> GetAllRecipesAsync();
+    Task<Recipe?> GetRecipeByIdAsync(int recipeId);
+    Task<IEnumerable<Recipe>> SearchRecipesByNameOrIngredientAsync(string query);
+    Task<IEnumerable<Recipe>> FilterRecipesAsync(
+        string? category,
+        string? cuisine,
+        int? minCalories,
+        int? maxCalories,
+        string? ingredient,
+        bool? withImage,
+        bool? withVideo);
+}
