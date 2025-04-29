ee315be820dea3b2021da57c193f09df01c6bf94
Jorune Sveikauskaite <jorune02@gmail.com>
2025-04-29 15:33:43 +0100

    Fix Meal Plan Repository

--- EAD2_CA2.Api/Repositories/Interfaces/IMealPlanRepository.cs
+++ EAD2_CA2.Api/Repositories/Interfaces/IMealPlanRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace EAD2_CA2.Api.Repositories.interfaces
+﻿using EAD2_CA2.Api.Entities;
+
+namespace EAD2_CA2.Api.Repositories.Interfaces;
+
+public interface IMealPlanRepository
 {
-    public class IMealPlanRepository
-    {
-    }
+    Task<IEnumerable<MealPlan>> GetAllMealPlansAsync();
+    Task<MealPlan?> GetMealPlanByIdAsync(int mealPlanId);
+    Task<IEnumerable<MealPlan>> GetMealPlanByDayAsync(Enums.Day dayofWeek);
+    Task<MealPlan> AddMealPlanAsync(MealPlan mealPlan);
+    Task<MealPlan> EditMealPlanAsync(MealPlan mealPlan);
+    Task DeleteMealPlanAsync(int mealPlanId);
 }
