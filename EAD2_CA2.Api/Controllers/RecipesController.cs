9a110aaa0242620bf7caba3e280668eb7ea114ed
Ken <keneithatillo@gmail.com>
2025-04-29 22:52:18 +0100

    feat: add recipe controller

---
+++ EAD2_CA2.Api/Controllers/RecipesController.cs
@@ -0,0 +1,63 @@
+﻿using EAD2_CA2.Api.Entities;
+using EAD2_CA2.Api.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EAD2_CA2.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RecipesController : Controller
+{
+    private readonly IRecipeRepository _recipeRepository;
+
+    public RecipesController(IRecipeRepository recipeRepository)
+    {
+        _recipeRepository = recipeRepository ?? throw new ArgumentNullException(nameof(recipeRepository));
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Recipe>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllRecipes()
+    {
+        var recipes = await _recipeRepository.GetAllRecipesAsync();
+
+        return Ok(recipes);
+    }
+
+    [HttpGet("{recipeId}")]
+    [ProducesResponseType(typeof(Recipe), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetRecipeById(int recipeId)
+    {
+        var recipe = await _recipeRepository.GetRecipeByIdAsync(recipeId);
+        if (recipe == null) return NotFound();
+
+        return Ok(recipe);
+    }
+
+    [HttpGet("search")]
+    [ProducesResponseType(typeof(IEnumerable<Recipe>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> SearchRecipesByNameOrIngredient([FromQuery] string query)
+    {
+        var recipes = await _recipeRepository.SearchRecipesByNameOrIngredientAsync(query);
+
+        return Ok(recipes);
+    }
+
+    [HttpGet("filter")]
+    [ProducesResponseType(typeof(IEnumerable<Recipe>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> FilterRecipes(
+    [FromQuery] string? category,
+    [FromQuery] string? cuisine,
+    [FromQuery] int? minCalories,
+    [FromQuery] int? maxCalories,
+    [FromQuery] string? ingredient,
+    [FromQuery] bool? withImage,
+    [FromQuery] bool? withVideo)
+    {
+        var recipes = await _recipeRepository
+            .FilterRecipesAsync(category, cuisine, minCalories, maxCalories, ingredient, withImage, withVideo);
+
+        return Ok(recipes);
+    }
+}
---
+++ EAD2_CA2.Api/Migrations/20250429215136_Initial.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EAD2_CA2.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MealPlans",
+                columns: table => new
+                {
+                    MealPlanId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1000, 1"),
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    DayOfWeek = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MealType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MealPlans", x => x.MealPlanId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "0, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cuisine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ingredients = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Instructions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VideoUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Calories = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.RecipeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MealPlans");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
