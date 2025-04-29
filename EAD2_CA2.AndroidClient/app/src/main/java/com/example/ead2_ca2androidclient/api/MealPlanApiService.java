5f9f9ee75b162732bddca28611e78fbd0f645869
Jorune Sveikauskaite <jorune02@gmail.com>
2025-04-30 00:54:49 +0100

    Meal Plan api service added

--- EAD2_CA2.AndroidClient/app/src/main/java/com/example/ead2_ca2androidclient/api/MealPlanApiService.java
+++ EAD2_CA2.AndroidClient/app/src/main/java/com/example/ead2_ca2androidclient/api/MealPlanApiService.java
@@ -1,4 +1,34 @@
 package com.example.ead2_ca2androidclient.api;
 
+import com.example.ead2_ca2androidclient.models.MealPlan;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface MealPlanApiService {
+    @GET("api/MealPlans")
+    Call<List<MealPlan>> getAllMealPlans();
+    
+    @GET("api/MealPlans")
+    Call<List<MealPlan>> getMealPlansByDay(@Query("day") String day);
+    
+    @GET("api/MealPlans/{mealPlanId}")
+    Call<MealPlan> getMealPlanById(@Path("mealPlanId") int mealPlanId);
+    
+    @POST("api/MealPlans")
+    Call<MealPlan> createMealPlan(@Body MealPlan mealPlan);
+    
+    @PUT("api/MealPlans/{mealPlanId}")
+    Call<MealPlan> updateMealPlan(@Path("mealPlanId") int mealPlanId, @Body MealPlan mealPlan);
+    
+    @DELETE("api/MealPlans/{mealPlanId}")
+    Call<Void> deleteMealPlan(@Path("mealPlanId") int mealPlanId);
 }
